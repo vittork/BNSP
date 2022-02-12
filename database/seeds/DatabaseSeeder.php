b78f40a1fa4f2d94101536217ea34b7a4d9a9909
Muhammad Fikri <fikri2347@gmail.com>
2022-02-12 17:46:30 +0700

    Membuat databse seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\User;
+use App\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +14,40 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        // Create new user
+        $user = new User;
+        $user->name     = 'admin';
+        $user->email    = 'admin@gmail.com';
+        $user->password = bcrypt('admin');
+        $user->save();
+
+
+        // Create new product
+        $products   = [
+            [
+                'name' => 'Superior Room',
+                'harga' => '500000',
+                'image' => 'SR.jpg',
+            ],
+            [
+                'name' => 'Deluxe Room',
+                'harga' => '1000000',
+                'image' => 'DR.jpg',
+            ],
+            [
+                'name' => 'Executive Room',
+                'harga' => '1500000',
+                'image' => 'ER.jpg',
+            ]
+        ];
+
+        foreach($products as $product) {
+            $newProduct = new Product;
+            $newProduct->name           = $product['name'];
+            $newProduct->harga          = $product['harga'];
+            $newProduct->img_type_room  = $product['image'];
+            $newProduct->save();
+        }
     }
 }
